d2d3bbccd96a08d775faa8a78a7dbdd8ab935339
agservizi <ag.servizi16@gmail.com>
2026-02-14 22:37:56 +0100

    Ottimizza il caricamento delle immagini nel sidebar utilizzando variabili per i loghi e fallback

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,12 +1,21 @@
+@php
+    $sidebarLogo = getFile(getSidebarLogo());
+    $sidebarLogoFallback = asset('uploads/logo/logo-dark.png');
+    $sidebarFav = getFile(getFavIcon());
+    $sidebarFavFallback = asset('uploads/logo/favicon.png');
+@endphp
+
 <nav
-       class="{{ $customThemeBackground == 'on' ? 'dash-sidebar light-sidebar transprent-bg' : 'dash-sidebar light-sidebar' }}">
+    class="{{ $customThemeBackground == 'on' ? 'dash-sidebar light-sidebar transprent-bg' : 'dash-sidebar light-sidebar' }}">
 
        <div class="navbar-wrapper">
            <div class="m-header main-logo">
                <a href="{{ route('admin.dashboard') }}" class="b-brand">
-                   <img src="{{ getFile(getSidebarLogo()) }}{{ '?' . time() }}"
+                   <img src="{{ $sidebarLogo }}"
+                       onerror="this.onerror=null;this.src='{{ $sidebarLogoFallback }}';"
                        alt="{{ config('app.name', 'TicketGo SaaS') }}" class="logo logo-lg">
-                       <img src="{{ getFile(getFavIcon()) }}{{ '?' . time() }}"
+                       <img src="{{ $sidebarFav }}"
+                       onerror="this.onerror=null;this.src='{{ $sidebarFavFallback }}';"
                        alt="{{ config('app.name', 'TicketGo SaaS') }}" class="logo logo-sm">
                </a>
            </div>
